5876a17878c28877545355ae00b9f082741970e4
Raissa Ferreira <rai200890@gmail.com>
2019-12-19 15:20:19 -0300

    feature: Allow handle_job to return something other than Ecto.Multi

--- test/support/test_job.ex
+++ test/support/test_job.ex
@@ -1,22 +1,56 @@
 defmodule EctoJobScheduler.Test.TestJob do
   @moduledoc false
-
   use EctoJobScheduler.Job,
     repo: EctoJobScheduler.Test.Repo,
     otp_app: :ecto_job_scheduler
 
-  alias Ecto.Multi
+  def handle_job(%JobInfo{multi: multi}, _params),
+    do: Ecto.Multi.run(multi, :test_job, fn _, _ -> {:ok, :xablau} end)
+end
+
+defmodule EctoJobScheduler.Test.TestJobError do
+  @moduledoc false
+  use EctoJobScheduler.Job,
+    repo: EctoJobScheduler.Test.Repo,
+    otp_app: :ecto_job_scheduler
 
   def handle_job(%JobInfo{multi: multi}, _params),
-    do: Multi.run(multi, :test_job, fn _, _ -> {:ok, :xablau} end)
+    do: Ecto.Multi.run(multi, :test_job, fn _, _ -> {:error, :xablau} end)
+end
+
+defmodule EctoJobScheduler.Test.TestJobNotMultiSuccessful do
+  @moduledoc false
+  use EctoJobScheduler.Job,
+    repo: EctoJobScheduler.Test.Repo,
+    otp_app: :ecto_job_scheduler
+
+  def handle_job(%JobInfo{multi: _multi}, _params) do
+    {:ok, :xablau}
+  end
+end
+
+defmodule EctoJobScheduler.Test.TestJobNotMultiError do
+  @moduledoc false
+  use EctoJobScheduler.Job,
+    repo: EctoJobScheduler.Test.Repo,
+    otp_app: :ecto_job_scheduler
+
+  def handle_job(%JobInfo{multi: _multi}, _params) do
+    {:error, :sad_keanu}
+  end
 end
 
 defmodule EctoJobScheduler.Test.TestJobQueue do
   @moduledoc false
   use EctoJobScheduler.JobQueue,
     table_name: "test_jobs",
-    jobs: [EctoJobScheduler.Test.TestJob],
-    max_attempts: 5
+    jobs: [
+      EctoJobScheduler.Test.TestJob,
+      EctoJobScheduler.Test.TestJobError,
+      EctoJobScheduler.Test.TestJobNotMultiSuccessful,
+      EctoJobScheduler.Test.TestJobNotMultiError
+    ],
+    max_attempts: "5"
 end
 
 defmodule EctoJobScheduler.Test.TestJobScheduler do
