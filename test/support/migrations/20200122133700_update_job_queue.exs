2a2bf48abc5d13bdfa2cc8d651a7d30d31011987
Raissa Ferreira <rai200890@gmail.com>
2020-01-22 17:18:58 -0300

    style: FIx credo warnings

--- test/support/migrations/20200122133700_update_job_queue.exs
+++ test/support/migrations/20200122133700_update_job_queue.exs
@@ -1,14 +1,16 @@
 defmodule EctoJobScheduler.Test.Repo.Migrations.UpdateJobQueue do
   use Ecto.Migration
 
+  alias EctoJob.Migrations.UpdateJobTable
+
   @ecto_job_version 3
   @table_name "test_jobs"
 
   def up do
-    EctoJob.Migrations.UpdateJobTable.up(@ecto_job_version, @table_name)
+    UpdateJobTable.up(@ecto_job_version, @table_name)
   end
 
   def down do
-    EctoJob.Migrations.UpdateJobTable.down(@ecto_job_version, @table_name)
+    UpdateJobTable.down(@ecto_job_version, @table_name)
   end
 end
